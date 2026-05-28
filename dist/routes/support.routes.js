8c7f780f837b04fcb0089a34eb55a6e5acb32922
Tunahan-labs <tunahankursun369@gmail.com>
2026-05-28 16:49:07 +0200

    remove .env and dist

--- dist/routes/support.routes.js
+++
@@ -1,6 +0,0 @@
-// src/routes/supportTicket.routes.ts
-import { Router } from "express";
-import { SupportTicketController } from "../controllers/support.controller.js";
-const router = Router();
-router.post("/support", SupportTicketController.submit);
-export default router;
